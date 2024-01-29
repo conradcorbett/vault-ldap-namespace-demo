fc7c5130e3abd02a9f9523d00feb400b13cf2e9a
conradcorbett <conrad.corbett@hashicorp.com>
2024-01-29 13:00:16 -0800

    vault server

---
+++ vault-server/variables.tf
@@ -0,0 +1,7 @@
+variable "docker_image" {
+  default = "hashicorp/vault-enterprise:1.13-ent"
+}
+
+variable "vault_license" {
+  default = "~/Applications/vault1.13.2entdarwinarm64/July2024.hclic"
+}
